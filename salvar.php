c0fc5c5f4198fcba4af211b0d4c9856cadd6c5b5
Polacompr <pasdioramurilo@gmail.com>
2026-08-25 11:08:36 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -1,11 +1,20 @@
 <?php
+
 include("conexao.php");
+
 $tarefa = $_POST['tarefa'];
+
 $sql = "INSERT INTO tarefas (tarefa)
- VALUES ('$tarefa')";
-if(mysqli_query($conexao,$sql)){
- header("Location: ../index.php");
-}else{
- echo "Erro ao salvar.";
+        VALUES ('$tarefa')";
+
+if (mysqli_query($conexao, $sql)) {
+
+    header("Location: ../index.php");
+
+} else {
+
+    echo "Erro ao salvar.";
+
 }
+
 ?>
