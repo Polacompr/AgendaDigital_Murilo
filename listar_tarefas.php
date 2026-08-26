1061a2fd44d20ad6b1760bc410c43f8d21271e62
Polacompr <pasdioramurilo@gmail.com>
2026-08-25 23:27:09 -0300

    Update listar_tarefas.php

--- listar_tarefas.php
+++ listar_tarefas.php
@@ -1,21 +1,15 @@
 <?php
-
 include 'conexao.php';
 
-$sql = "SELECT * FROM Tarefa ORDER BY id_tarefa DESC";
-
+$sql = "SELECT * FROM tarefas ORDER BY id DESC";
 $resultado = $conexao->query($sql);
 
-$tarefas = array();
-
-while ($linha = $resultado->fetch_assoc()) {
-
-    $tarefas[] = $linha;
-
+$tarefas = [];
+if ($resultado) {
+    while ($linha = $resultado->fetch_assoc()) {
+        $tarefas[] = $linha;
+    }
 }
 
 header('Content-Type: application/json');
-
 echo json_encode($tarefas);
-
-?>
